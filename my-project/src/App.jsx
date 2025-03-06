d38afce9649d4354fcbab0e8cff59bc4662e18b1
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-06 12:39:53 +0600

    gadge haven project start

---
+++ my-project/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      <button className='btn btn-warning'>btn</button>
+      
+    </>
+  )
+}
+
+export default App
---
+++ my-project/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const routes=createBrowserRouter([
+  {
+    path:'/',
+    element:<App />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={routes}/>
+  </StrictMode>,
+)
